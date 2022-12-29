df51885760c3bef9c2bcb8abe394e121776e1a2c
Jay Gaha <jaygaha@gmail.com>
2022-12-29 12:49:09 +0900

    support page fixed

--- support.php
+++ support.php
@@ -3,10 +3,10 @@ session_start();
 require_once("includes/db.php");
 require_once("functions/mailer.php");
 require_once("social-config.php");
-if(isset($_GET['view_tickets'])){
+if (isset($_GET['view_tickets'])) {
 	include_once('ticket_support/view_tickets.php');
-}elseif(isset($_GET['new_ticket'])){
+} elseif (isset($_GET['new_ticket'])) {
 	include_once('ticket_support/new_ticket.php');
-}elseif(isset($_GET['view_conversation'])){
+} elseif (isset($_GET['view_conversation'])) {
 	include_once('ticket_support/view_conversation.php');
 }
